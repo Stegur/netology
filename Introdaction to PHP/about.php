2dde26370110628ea6b4722b101b105832f5ea72
Alex Stegura <stegura.alex@gmail.com>
2018-06-24 21:16:53 +0300

    Delete .idea

--- Introdaction to PHP/about.php
+++ Introdaction to PHP/about.php
@@ -1,9 +1,9 @@
 <?php
-    $name = Алексей;
-    $age = 27;
-    $city = "Симферополь";
-    $email = "stegura.alex@gmail.com";
-    $about = "Full stack web developer";
+$name = "Алексей";
+$age = 27;
+$city = "Симферополь";
+$email = "stegura.alex@gmail.com";
+$about = "Full stack web developer";
 ?>
 
 <!doctype html>
@@ -14,8 +14,13 @@
     <title>About</title>
 </head>
 <body>
-    <main>
-        <h1>Всем привет! Меня зовут <?= $name ?></h1>
-    </main>
+<div style="width: 800px; height: 600px; margin: auto; text-align: center; box-shadow: 0 0  20px black;">
+    <h1>Всем привет! Меня зовут <?= $name ?></h1>
+    <p>Это моя первая страничка на PHP.</p>
+    <p>Я бы хотел немного рассказать о себе:
+        мне <?= $age ?> лет, Я живу в г. <?= $city ?></p>
+    <p>Я учусь на <?= $about ?> в онлайн-университете "Нетология"</p>
+    <p>Если Вас заинтересовали <a href="https://github.com/Stegur">мои работы</a>, связаться со мной можно по электронной почте: <?= $email ?></p>
+</div>
 </body>
 </html>
