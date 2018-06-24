01f83c7ecd7783d7deac0add1840d7bfc8896ace
Alex Stegura <stegura.alex@gmail.com>
2018-06-24 13:56:59 +0300

    Rename main folder

---
+++ Introdaction to PHP/about.php
@@ -0,0 +1,21 @@
+<?php
+    $name = Алексей;
+    $age = 27;
+    $city = "Симферополь";
+    $email = "stegura.alex@gmail.com";
+    $about = "Full stack web developer";
+?>
+
+<!doctype html>
+<html lang="ru-en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="styles.css">
+    <title>About</title>
+</head>
+<body>
+    <main>
+        <h1>Всем привет! Меня зовут <?= $name ?></h1>
+    </main>
+</body>
+</html>
