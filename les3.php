c36d2952806d6475663b0eaa09c4a2e7eeafba1b
Alex Stegura <stegura.alex@gmail.com>
2018-06-28 16:09:23 +0300

    add switch

--- les3.php
+++ les3.php
@@ -1,16 +1,30 @@
 <?php
 
-$x = 1;
+//$x = 1;
+//
+//if($x > 1){
+//    echo "X > 1";
+//}elseif($x == 1) {
+//    echo "X = 1";
+//}else{
+//    echo "X < 1";
+//}
+//
+////var_dump($x >= 1)
 
-if($x > 1){
-    echo "X > 1";
-}elseif($x == 1) {
-    echo "X = 1";
-}else{
-    echo "X < 1";
+$b = 7;
+
+switch ($b){
+    case 5:
+        echo "$b";
+        break;
+
+    case 1:
+        echo "1-2";
+        break;
+
+    default:
+        echo "default";
+        break;
 }
-
-//var_dump($x >= 1)
-
-
 ?>
