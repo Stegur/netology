3af139e722068715212be8afb938fa919d3494fd
Alex Stegura <stegura.alex@gmail.com>
2018-07-28 22:42:14 +0300

    Автоматические сеттеры и геттеры

--- les12.php
+++ les12.php
@@ -1,14 +1,40 @@
 <?php
-class Test
+//class Test
+//{
+//    private $private = 10;
+//    public function __get($name)
+//    {
+////       echo 'Вы обратились к несуществующему свойству - ' . $name;
+//        if (isset($this->$name))
+//            return $this->$name;
+//    }
+//    public function __set($name, $value)
+//    {
+//        echo 'Ошибка присвоения значения. Свойство ' . $name . ' недоступно';
+//    }
+//}
+
+//$test = new Test();
+//echo $test->private;
+//$test->some = 20;
+
+class TestClass
 {
-    private $private = 10;
+    private $data = [];
+    public function __set($name, $value)
+    {
+        $this->data[$name] = $value;
+    }
+    
     public function __get($name)
     {
-//       echo 'Вы обратились к несуществующему свойству - ' . $name;
-        if (isset($this->$name))
-            return $this->$name;
+        if (isset($this->data[$name]))
+            return $this->data[$name];
     }
 }
 
-$test = new Test();
-echo $test->private;
+$test = new TestClass();
+$test->lalal = 5;
+echo '<pre>';
+var_dump($test);
+echo $test->lalal;
